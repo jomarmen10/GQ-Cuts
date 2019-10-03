a06d5317e04de965700de9fe991606acdfba6c80
Jomar Mendoza <jmm21@Jomars-MacBook-Pro.local>
2019-10-03 10:46:45 -0700

    navbar is created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Navbar/>
+
     </div>
   );
 }
