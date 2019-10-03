e7356b1de6d87575fdf40bf52d270e1203e2d4c9
Jomar Mendoza <jmm21@Jomars-MacBook-Pro.local>
2019-10-03 13:54:22 -0700

    pricing component have basic info and design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
-
+import { NavRow } from '../Style/Style';
 
 
 
 const Navbar = props => {
   return (
-    <div>
-      <h1>GQ Cuts</h1>
-      <NavLink to={'/'}>Home</NavLink>
-      <NavLink to={'/schedule'}>schedule</NavLink>
-      <NavLink to={'/pricing'}>pricing</NavLink>
-      <NavLink to={'/gallery'}>gallery</NavLink>
-      <NavLink to={'/barbers'}>barbers</NavLink>
-    </div>
+    <NavRow>
+      <div className="container">
+        <h1 className='left'>GQ Cuts</h1>
+        <div className='right'>
+          <NavLink to={'/'}>Home</NavLink>
+          <NavLink to={'/schedule'}>schedule</NavLink>
+          <NavLink to={'/pricing'}>pricing</NavLink>
+          <NavLink to={'/gallery'}>gallery</NavLink>
+          <NavLink to={'/barbers'}>barbers</NavLink>
+        </div>
+      </div>
+    </NavRow>
   )
 }
 
---
+++ src/components/Style/Style.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+
+export const NavRow = styled.div`
+  .container {
+    width: 1000px;
+    margin: 0 auto;
+  }
+
+  a {
+    color: black;
+    margin: 25px;
+    text-decoration: none;
+  }
+
+  .left {
+    float: left
+  }
+
+  .right {
+    float: right;
+    padding-top: 30px;
+  }
+
+  height: 100px;
+`
+
+export const PricingStyle = styled.div`
+  .container {
+    width: 1000px;
+    margin: 0 auto;
+  }
+
+  .center {
+    text-align: center;
+    padding-top: 50px;
+  }
+
+  .left {
+    float: left;
+  }
+
+  .right {
+    float: right;
+  }
+`
